c559f541056f05177c62b9fd4276824a4538cc94
Ahmadreza <ahmadrezayazdinejadf.d2000@gmail.com>
2022-12-03 22:28:11 +0330

    skincolor

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
   print(
-    'Name:ahmadreza,lastName:Yazdinejad,job:developer,height:173 ,wight:54',
+    'Name:ahmadreza,lastName:Yazdinejad,job:developer,height:173 ,wight:54,skinColor:White',
   );
 }
